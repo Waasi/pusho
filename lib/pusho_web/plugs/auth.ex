1766bcb3c49deddcbfc6af91382dae253d14bc55
Eric Santos <eric.leinad92@gmail.com>
2018-09-28 16:24:22 -0400

    Feature[Plugs] implemented signature verification auth plug

---
+++ lib/pusho_web/plugs/auth.ex
@@ -0,0 +1,34 @@
+defmodule PushoWeb.Plugs.Auth do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, [api_key: key]) do
+    with body = format(conn),
+         signed_body = sign(body, key),
+         %{body_params: %{"signature" => signature}} <- conn,
+         true <- signature == signed_body do
+      conn
+    else
+      _invalid_request ->
+        response_body = Poison.encode!(%{error: "unauthorized request"})
+
+        conn
+        |> send_resp(403, response_body)        
+        |> halt()
+    end
+  end
+
+  defp format(%{body_params: body_params}) do
+    body_params
+    |> Map.drop(["signature"])
+    |> Poison.encode!()
+  end
+  defp format(_conn), do: ""
+
+  defp sign(body, key) do
+    :sha256
+    |> :crypto.hmac(key, body)
+    |> Base.encode16()
+  end
+end
